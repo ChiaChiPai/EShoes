c3b4383e0fbaea459bc6953a587744db6c103f8a
White Pai <white_pai@appdet.com>
2021-07-23 22:59:54 +0800

    fix(pwa): remove pwa config

--- vite.config.ts
+++ vite.config.ts
@@ -79,36 +79,6 @@ export default defineConfig({
     WindiCSS({
       safelist: markdownWrapperClasses,
     }),
-
-    // https://github.com/antfu/vite-plugin-pwa
-    VitePWA({
-      registerType: 'autoUpdate',
-      includeAssets: ['favicon.svg', 'robots.txt', 'safari-pinned-tab.svg'],
-      manifest: {
-        name: 'Vitesse',
-        short_name: 'Vitesse',
-        theme_color: '#ffffff',
-        icons: [
-          {
-            src: '/pwa-192x192.png',
-            sizes: '192x192',
-            type: 'image/png',
-          },
-          {
-            src: '/pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-          },
-          {
-            src: '/pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'any maskable',
-          },
-        ],
-      },
-    }),
-
     // https://github.com/intlify/vite-plugin-vue-i18n
     VueI18n({
       runtimeOnly: true,
